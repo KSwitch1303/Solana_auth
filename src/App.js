b4ee149e3d3a8844c6c412a444ff95a64acbc9d4
KSwitch1303 <ebimieteimisongo@gmail.com>
2022-11-08 17:56:53 +0100

    master commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-
-const App = () =>  {
+// import { PhantomWalletAdapter } from './index2.ts';
+const App = () => {
+  //solana.on("connect", () => console.log("connected!"));
   const [walletAddress, setWalletAddress] = useState(null);
+  const { solana } = window;
   const checkIfWalletIsConnected = async () => {
     try {
-      const { solana } = window;
+       const { solana } = window;
       if (solana) {
         if (solana.isPhantom) {
-        console.log('Phantom wallet found!');
-        const response = await solana.connect({ onlyIfTrusted: true });
-        console.log('Connected with Public Key:', response.publicKey.toString());
-        const wallet = response.publicKey.toString()
-        setWalletAddress(response.publicKey.toString()); 
-      }
+          console.log('Phantom wallet found!');
+          const response = await solana.connect({ onlyIfTrusted: true });
+          console.log('Connected with Public Key:', response.publicKey.toString());
+          const wallet = response.publicKey.toString()
+          setWalletAddress(response.publicKey.toString());
+        }
       } else {
         alert('Solana object not found! Get a Phantom Wallet 👻');
       }
@@ -22,12 +24,24 @@ const App = () =>  {
     }
   };
   const connectWallet = async () => {
-    const { solana } = window;
+    // const { solana } = window;
     if (solana) {
       const response = await solana.connect();
       console.log('Connected with Public Key:', response.publicKey.toString());
       setWalletAddress(response.publicKey.toString());
-    } 
+      console.log(solana.isConnected);
+    }
+  };
+
+
+  const disconnected = async () => {
+    // const { solana } = window;
+    solana.disconnect();
+    console.log(solana.isConnected);
+    solana.on("disconnect", () => {
+     setWalletAddress(null);
+      console.log(solana.isConnected);
+    })
   };
   const renderNotConnectedContainer = () => (
     <button className="cta-button connect-wallet-button" onClick={connectWallet}>
@@ -35,7 +49,7 @@ const App = () =>  {
     </button>
   );
   const renderConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button className="cta-button connect-wallet-button" onClick={disconnected}>
       Wallet Already Connected. Revoke connection from your Phantom wallet
     </button>
   );
@@ -47,19 +61,16 @@ const App = () =>  {
     window.addEventListener('load', onLoad);
     return () => window.removeEventListener('load', onLoad);
   }, []);
-
-
-
+  
   return (
     <div className="App">
       <header className="App-header">
         <p>
-        {!walletAddress && renderNotConnectedContainer()}
-        {walletAddress && renderConnectedContainer()}
+          {!walletAddress && renderNotConnectedContainer()}
+          {walletAddress && renderConnectedContainer()}
         </p>
       </header>
     </div>
   );
 }
-
 export default App;
